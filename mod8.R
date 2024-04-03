cc57c6d74a5a184b4e93457ee00f22a440cbf0eb
Nazif Dilmen <157618507+NazifD@users.noreply.github.com>
2024-04-03 04:25:32 -0400

    Update mod8.R

--- mod8.R
+++ mod8.R
@@ -2,21 +2,20 @@ rm(list=ls())
 install.packages('gapminder')
 library(gapminder)
 
-# Get the latest year in the dataset
 latest_year <- max(gapminder$year)
 
-# Box plots (grouped by continent) for GDP per capita and life expectancy
+Box plots (
 boxplot(gdpPercap = continent, data = subset(gapminder, year == latest_year), main = "GDP per capita by continent (latest year)")
 boxplot(lifeExp = continent, data = subset(gapminder, year == latest_year), main = "Life expectancy by continent (latest year)")
 
-# Time series of mean GDP per capita and life expectancy by continent
+# Time series 
 plot(tapply(gapminder$gdpPercap, list(gapminder$year, gapminder$continent), mean), xlab = "Year", ylab = "Mean GDP per capita", main = "Mean GDP per capita over time by continent", type = "l")
 legend("topleft", legend = levels(gapminder$continent), lty = 1)
 
 plot(tapply(gapminder$lifeExp, list(gapminder$year, gapminder$continent), mean), xlab = "Year", ylab = "Mean life expectancy", main = "Mean life expectancy over time by continent", type = "l")
 legend("bottomright", legend = levels(gapminder$continent), lty = 1)
 
-# Scatterplots of life expectancy vs GDP per capita, colored by continent
+# Scatterplots of life expectancy vs GDP per capita
 plot(gapminder$gdpPercap, gapminder$lifeExp, main = "Life expectancy vs GDP per capita", xlab = "GDP per capita", ylab = "Life expectancy", pch = 16, cex = gapminder$pop / 1e6)
 legend("bottomright", legend = levels(gapminder$continent), pch = 16)
 
@@ -27,3 +26,9 @@ legend("bottomright", legend = levels(gapminder$continent), pch = 16)
 plot(gapminder$gdpPercap, log(gapminder$lifeExp), main = "log(Life expectancy) vs GDP per capita", xlab = "GDP per capita", ylab = "log(Life expectancy)", pch = 16, cex = gapminder$pop / 1e6)
 legend("bottomright", legend = levels(gapminder$continent), pch = 16)
 
+ggsave("scatterplot_original.png", plot = p1, width = 10, height = 6)
+ggsave("scatterplot_log.png", plot = p2, width = 10, height = 6)
+png("boxplots.png", width = 800, height = 600)
+png("time_series.png", width = 800, height = 600)
+
+  
